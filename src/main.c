0d96af61934448cc8a632d04533b488d96043979
adrian-brady <mail@adrianbrady.com>
2024-09-06 23:42:51 -0500

    add program

---
+++ src/main.c
@@ -0,0 +1,371 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 800
+
+#define COUNT 40
+#define SCALE (1.0/COUNT)
+#define SIZE (int)(WINDOW_WIDTH * SCALE)
+#define LEFT_COLUMN_BOUND 0
+#define RIGHT_COLUMN_BOUND (WINDOW_WIDTH - SIZE)
+#define TOP_ROW_BOUND 0
+#define BOTTOM_ROW_BOUND (WINDOW_HEIGHT - SIZE)
+
+
+typedef struct Bar {
+    int pos;
+    int row;
+    int width;
+} Bar;
+
+enum VerticalDirection {
+    UP,
+    DOWN,
+    V_NONE,
+};
+
+enum HorizontalDirection {
+    RIGHT,
+    LEFT,
+    H_NONE,
+};
+
+typedef struct Color {
+    Uint8 red;
+    Uint8 green;
+    Uint8 blue;
+    Uint8 alpha;
+} Color;
+
+int getPixel(int pixel) {
+    return (pixel * SIZE);
+}
+
+void storeColor(SDL_Renderer* renderer, Color* colors) {
+    SDL_GetRenderDrawColor(renderer, &colors->red, &colors->green, &colors->blue, &colors->alpha);
+}
+
+void restoreColor(SDL_Renderer* renderer, Color* colors) {
+    SDL_SetRenderDrawColor(renderer, colors->red, colors->green, colors->blue, colors->alpha);
+}
+
+typedef struct Ball {
+    SDL_Rect rect;
+    enum VerticalDirection vdir;
+    enum HorizontalDirection hdir;
+    int pos_x_bound;
+    int pos_y_bound;
+    int neg_x_bound;
+    int neg_y_bound;
+} Ball;
+
+void drawVerticalLine(int pos, SDL_Renderer* renderer) {
+    SDL_Rect rect;
+    rect.x = pos;
+    rect.w = SIZE;
+    rect.h = SIZE;
+
+    for (int i = 0; i < WINDOW_HEIGHT; i += SIZE) {
+        rect.y = i;
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+
+void drawHorizontalLine(int pos, SDL_Renderer* renderer) {
+    SDL_Rect rect;
+    rect.y = pos;
+    rect.w = SIZE;
+    rect.h = SIZE;
+
+    for (int i = 0; i < WINDOW_WIDTH; i += SIZE) {
+        rect.x = i;
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+
+bool checkCollision(Bar* bar, Ball* ball);
+
+void updateBallPos(Ball* ball, SDL_Renderer* renderer, Bar* bar) {
+    switch (ball->vdir) {
+        case UP:
+            if (ball->rect.y - SIZE > ball->neg_y_bound) {
+                ball->rect.y -= SIZE;
+            } else {
+                ball->vdir = DOWN;
+                ball->rect.y += SIZE;
+            }
+            break;
+        case DOWN:
+            if (ball->rect.y + SIZE < getPixel((bar->row / SIZE) + 5)) {
+                ball->rect.y += SIZE;
+            } else {
+                // game ends
+                ball->rect.y = -100;
+                break;
+            }
+            break;
+        case V_NONE:
+            break;
+    }
+
+    switch (ball->hdir) {
+        case RIGHT:
+            if (ball->rect.x + SIZE < ball->pos_x_bound) {
+                ball->rect.x += SIZE;
+            } else {
+                ball->hdir = LEFT;
+                ball->rect.x -= SIZE;
+            }
+            break;
+        case LEFT:
+            if (ball->rect.x - SIZE > ball->neg_x_bound) {
+                ball->rect.x -= SIZE;
+            } else {
+                ball->hdir = RIGHT;
+                ball->rect.x += SIZE;
+            }
+            break;
+        case H_NONE:
+            break;
+    }
+    if (checkCollision(bar, ball)) {
+        switch (ball->vdir) {
+            case UP:
+                ball->vdir = DOWN;
+                break;
+            case DOWN:
+                ball->vdir = UP;
+                break;
+            case V_NONE:
+                break;
+        }
+
+        switch (ball->hdir) {
+            case RIGHT:
+                ball->hdir = LEFT;
+                break;
+            case LEFT:
+                ball->hdir = RIGHT;
+                break;
+            case H_NONE:
+                break;
+
+        }
+    }
+}
+
+void drawBall(Ball* ball, SDL_Renderer* renderer) {
+
+    Color old;
+    storeColor(renderer, &old);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &ball->rect);
+
+    restoreColor(renderer, &old);
+}
+
+double delta;
+
+Ball initBall(enum VerticalDirection vdir, enum HorizontalDirection hdir) {
+    Ball ball;
+    ball.vdir = vdir;
+    ball.hdir = hdir;
+    ball.rect.x = 0;
+    ball.rect.y = 0;
+    ball.rect.h = SIZE;
+    ball.rect.w = SIZE;
+    ball.neg_x_bound = LEFT_COLUMN_BOUND;
+    ball.neg_y_bound = TOP_ROW_BOUND;
+    ball.pos_x_bound = RIGHT_COLUMN_BOUND;
+    ball.pos_y_bound = BOTTOM_ROW_BOUND;
+
+    return ball;
+}
+
+void initBar(Bar* bar) {
+    bar->row = getPixel((WINDOW_HEIGHT / SIZE)-5);
+    bar->width = 5;
+    bar->pos = (WINDOW_WIDTH - getPixel(bar->width)) / 2;
+    bar->pos = getPixel((bar->pos/SIZE) + 4);
+}
+
+void drawBar(SDL_Renderer* renderer, Bar* bar) {
+    Color old;
+    storeColor(renderer, &old);
+
+    SDL_Rect rect;
+    rect.x = bar->pos;
+    rect.y = bar->row;
+    rect.w = bar->width * SIZE;
+    rect.h = SIZE;
+
+    // printf("x: %d, y: %d, w: %d, h: %d\n", rect.x, rect.y, rect.w, rect.h);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+    SDL_RenderFillRect(renderer, &rect);
+
+    restoreColor(renderer, &old);
+}
+
+bool isInBounds(int b1, int b2, int p1) {
+    if (p1 >= b1 && p1 <= b2)
+        return true;
+    return false;
+}
+
+bool checkCollision(Bar* bar, Ball* ball) {
+    int ball_x;
+    if (ball->hdir == RIGHT) {
+        ball_x = ball->rect.x + SIZE;
+    } else if (ball->hdir == LEFT) {
+        ball_x = ball->rect.x - SIZE;
+    } else {
+        ball_x = ball->rect.x;
+    }
+
+    int ball_y;
+    if (ball->vdir == UP) {
+        ball_y = ball->rect.y - SIZE;
+    } else if (ball->vdir == DOWN) {
+        ball_y = ball->rect.y + SIZE;
+    } else {
+        ball_y = ball->rect.y;
+    }
+
+    bool xBound = isInBounds(bar->pos, bar->pos+(bar->width*SIZE), ball_x);
+    bool yBound = isInBounds(bar->row, bar->row+SIZE, ball_y);
+    if (xBound && yBound) {
+        printf("bound by both x and y\n");
+        return true;
+    } else if (xBound) {
+        printf("bound by x\n");
+    } else if (yBound) {
+        printf("bound by y\n");
+    } else {
+        printf("not bound\n");
+    }
+
+    return false;
+}
+
+void barLeft(Bar* bar) {
+    if (bar->pos - SIZE > LEFT_COLUMN_BOUND) {
+        bar->pos = getPixel((bar->pos/SIZE)-1);
+    }
+}
+
+void barRight(Bar* bar) {
+    if (bar->pos + SIZE < RIGHT_COLUMN_BOUND) {
+        bar->pos = getPixel((bar->pos/SIZE)+1);
+    }
+}
+
+bool KEYS[322] = {0};
+
+bool loop = true;
+
+void keyboard() {
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+        // check for messages
+        switch (event.type) {
+            // exit if the window is closed
+            case SDL_QUIT:
+                loop = false; // set game state to done,(do what you want here)
+                break;
+            // check for keypresses
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym < 322 && event.key.keysym.sym >= 0)
+                    KEYS[event.key.keysym.sym] = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void clearKeys() {
+    for (int i = 0; i < 322; i++) {
+        KEYS[0] = false;
+    }
+}
+
+Bar* gbar;
+void handleInput() {
+    if (KEYS[SDLK_d]) {
+        barRight(gbar);
+        KEYS[SDLK_d] = false;
+    }
+    if (KEYS[SDLK_a]) {
+        barLeft(gbar);
+        KEYS[SDLK_a] = false;
+    }
+    if (KEYS[SDLK_ESCAPE] || KEYS[SDLK_q]) {
+        loop = false;
+    }
+}
+
+int main() {
+    SDL_Event event;
+    SDL_Renderer *renderer = NULL;
+    SDL_Window *window = NULL;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_Init(SDL_INIT_VIDEO);
+
+
+    unsigned int a, b = SDL_GetTicks(), SDL_GetTicks();
+
+    gbar = (Bar*)malloc(sizeof(Bar));
+    Bar* bar = gbar;
+    Ball ball = initBall(DOWN, RIGHT);
+    initBar(bar);
+    bool play = true;
+
+    while (loop) {
+
+
+        a = SDL_GetTicks();
+        delta = a - b;
+        keyboard();
+
+
+        if (delta > 1000 / 10.0 && play) {
+            handleInput();
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            printf("fps: %f\n", 1000 / delta);
+
+            drawVerticalLine(0, renderer);
+            drawVerticalLine(getPixel(COUNT - 1), renderer);
+            drawHorizontalLine(0, renderer);
+
+
+            updateBallPos(&ball, renderer, bar);
+            drawBar(renderer, bar);
+
+            b = a;
+
+            drawBall(&ball, renderer);
+            // printf("x: %d, y: %d\n", ball.rect.x, ball.rect.y);
+            if (ball.rect.y < 0) {
+                play = false;
+                printf("You Lost!\n");
+            }
+            SDL_RenderPresent(renderer);
+        }
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
