c37d0f6f4e6dc8369ea796fbff34b45cacc1599a
adrian-brady <mail@adrianbrady.com>
2024-09-07 18:01:26 -0500

    refactor: separate game global state to own file

---
+++ include/game.h
@@ -0,0 +1,12 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "types.h"
+
+extern Bar* gbar;
+extern bool gloop;
+
+void init_global_bar();
+void init_global_loop();
+
+#endif
---
+++ src/game.c
@@ -0,0 +1,4 @@
+#include "game.h"
+
+void init_global_loop() { gloop = true; }
+void init_global_bar() { gbar = (Bar *)malloc(sizeof(Bar)); }
